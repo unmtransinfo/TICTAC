b1dee91eaf2ff09f6e8320ad200b3c51de633dc1
Jeremy Yang <jeremyjyang@gmail.com>
2020-10-09 15:08:10 -0600

    Ok.

--- sh/Go_xref_drugs.sh
+++ sh/Go_xref_drugs.sh
@@ -27,6 +27,9 @@ cat $DATADIR/aact_drugs_smi_pubchem_cid.tsv \
 	|sort -nu \
 	>$DATADIR/aact_drugs_smi_pubchem.cid
 #
+# Although SMILES not canonical, we preserve their precise text, to
+# allow AACT to PubChem mapping.
+#
 n_smi=$(cat $DATADIR/aact_drugs_smi.smi |wc -l)
 printf "SMILES (from LeadMine): %d\n" "${n_smi}"
 n_cid=$(cat $DATADIR/aact_drugs_smi_pubchem.cid |wc -l)
@@ -45,8 +48,7 @@ python3 -m BioClients.util.pandas.Utils selectcols --coltags "InChIKey" \
 	|sed -e '1d' |sed -e 's/"//g' \
 	>$DATADIR/aact_drugs_smi_pubchem.ink
 #
-n_ink=$(cat $DATADIR/aact_drugs_smi_pubchem.ink |wc -l)
-printf "InChIKeys (from PubChem): %d\n" ${n_ink}
+printf "InChIKeys (from PubChem): %d\n" $(cat $DATADIR/aact_drugs_smi_pubchem.ink |wc -l)
 ###
 python3 -m BioClients.chembl.Client get_mol_by_inchikey \
 	--i $DATADIR/aact_drugs_smi_pubchem.ink \
@@ -57,33 +59,27 @@ python3 -m BioClients.util.pandas.Utils selectcols --coltags "molecule_chembl_id
 	|sed -e '1d' |sort -u \
 	>$DATADIR/aact_drugs_ink2chembl.chemblid
 #
-n_chembl_mol=$(cat $DATADIR/aact_drugs_ink2chembl.chemblid |wc -l)
-printf "Mols (from ChEMBL): %d\n" ${n_chembl_mol}
+printf "Mols (from ChEMBL): %d\n" $(cat $DATADIR/aact_drugs_ink2chembl.chemblid |wc -l)
 #
 ###
 python3 -m BioClients.chembl.Client get_activity_by_mol \
 	--i $DATADIR/aact_drugs_ink2chembl.chemblid \
 	--o $DATADIR/aact_drugs_chembl_activity.tsv
 #
-n_chembl_act=$(cat $DATADIR/aact_drugs_chembl_activity.tsv |sed -e '1d' |wc -l)
-printf "Activities (from ChEMBL): %d\n" ${n_chembl_act}
+printf "Activities (from ChEMBL): %d\n" $(cat $DATADIR/aact_drugs_chembl_activity.tsv |sed -e '1d' |wc -l)
 #
 python3 -m BioClients.util.pandas.Utils selectcols --coltags "target_chembl_id" \
 	--i $DATADIR/aact_drugs_chembl_activity.tsv \
 	|sed -e '1d' |sort -u \
 	>$DATADIR/aact_drugs_chembl_target.chemblid
 #
-n_chembl_tgt=$(cat $DATADIR/aact_drugs_chembl_target.chemblid |wc -l)
-printf "Targets (from ChEMBL): %d\n" ${n_chembl_tgt}
+printf "Targets (from ChEMBL): %d\n" $(cat $DATADIR/aact_drugs_chembl_target.chemblid |wc -l)
 #
 python3 -m BioClients.chembl.Client get_target_components \
 	--i $DATADIR/aact_drugs_chembl_target.chemblid \
 	--o $DATADIR/aact_drugs_chembl_target_component.tsv
 #
-n_chembl_tgtc=$(python3 -m BioClients.util.pandas.Utils selectcols --coltags "component_id" \
-	--i $DATADIR/aact_drugs_chembl_target_component.tsv \
-	|sed -e '1d' |wc -l)
-printf "Target components (from ChEMBL): %d\n" ${n_chembl_tgtc}
+printf "Target components (from ChEMBL): %d\n" $(python3 -m BioClients.util.pandas.Utils selectcols --coltags "component_id" --i $DATADIR/aact_drugs_chembl_target_component.tsv |sed -e '1d' |wc -l)
 ###
 # 
 python3 -m BioClients.util.pandas.Utils selectcols --coltags "document_chembl_id" \
@@ -91,19 +87,15 @@ python3 -m BioClients.util.pandas.Utils selectcols --coltags "document_chembl_id
 	|sed -e '1d' |sort -u \
 	>$DATADIR/aact_drugs_chembl_document.chemblid
 #
-n_chembl_doc=$(cat $DATADIR/aact_drugs_chembl_document.chemblid |wc -l)
-printf "Documents (from ChEMBL): %d\n" ${n_chembl_doc}
+printf "Documents (from ChEMBL): %d\n" $(cat $DATADIR/aact_drugs_chembl_document.chemblid |wc -l)
 #
 python3 -m BioClients.chembl.Client get_document \
 	--i $DATADIR/aact_drugs_chembl_document.chemblid \
 	--o $DATADIR/aact_drugs_chembl_document.tsv
 #
-n_chembl_pmid=$(python3 -m BioClients.util.pandas.Utils selectcols --coltags "pubmed_id" \
-	--i $DATADIR/aact_drugs_chembl_document.tsv \
-	|sed -e '1d' |wc -l)
-printf "PubMed IDs (from ChEMBL): %d\n" ${n_chembl_pmid}
+printf "PubMed IDs (from ChEMBL): %d\n" $(python3 -m BioClients.util.pandas.Utils selectcols --coltags "pubmed_id" --i $DATADIR/aact_drugs_chembl_document.tsv |sed -e '1d' |wc -l)
 #
-python3 -m BioClients.pubchem.Client list_sources_assay \
+python3 -m BioClients.chembl.Client list_sources \
 	--o $DATADIR/chembl_sources.tsv
 #
 date
